f02d2037de1aa07834742807832f7f22f3ada43c
ignatov <sergey@ignatov.spb.su>
2010-02-04 17:52:27 +0300

    cleanup

--- src/parser/ExprTree.scala
+++ src/parser/ExprTree.scala
@@ -10,7 +10,7 @@ class ExprTree
 abstract class Expression extends ExprTree
 case class Number(value: Int) extends Expression
 case class AttributeOccurrence(value: String, sub: String) extends Expression
-case class Operator(left: ExprTree, right: ExprTree, op: String) extends Expression
+case class Operator(left: Expression, right: Expression, op: String) extends Expression
 
 abstract case class Relation(name: String) extends ExprTree
 case class Scheme(override val name: String, condition: IfExpr, attributes: List[AttributeDef], fls: List[FL]) extends Relation(name)
@@ -18,7 +18,7 @@ case class Task(override val name: String, scheme: String, in: List[ExprTree], o
 
 case class Package(name: String, lst: List[Relation]) extends ExprTree
 case class Block(attributes: List[AttributeDef], fls: List[FL]) extends ExprTree
-case class IfExpr(condition: ExprTree, positive: Block, negative: Block) extends ExprTree
-case class Type(name: String) extends ExprTree
+case class IfExpr(condition: Expression, positive: Block, negative: Block) extends ExprTree
+case class FL(result: AttributeOccurrence, implementation: Expression) extends ExprTree
 case class AttributeDef(name: String, t: Type) extends ExprTree
-case class FL(result: AttributeOccurrence, implementation: ExprTree) extends ExprTree
+case class Type(name: String) extends ExprTree

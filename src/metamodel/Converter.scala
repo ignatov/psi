d165fd823daf68cf2948f41470e9d69b20c861f3
ignatov <sergey@ignatov.spb.su>
2010-02-08 15:39:39 +0300

    provides attribute table to all Converter methods

--- src/metamodel/Converter.scala
+++ src/metamodel/Converter.scala
@@ -16,13 +16,12 @@ object Converter {
   val relationTable = new HashMap[String, R]()
 
   /**
-   * @param `pack' the package to be converted
+   * @param `pack' the package to be converted 
    */
   def run(pack: Package): P = package2P(pack)
 
   private def package2P(pack: Package): P = {
     pack.lst foreach (s => relationTable.put(s.name, relation2R(s)))
-    relationTable -= "#"
     P(pack.name, relationTable)
   }
 
@@ -35,18 +34,18 @@ object Converter {
       attributes foreach (
         a => attributeTable(a.name) = A(a.name, typeTable.getOrElseUpdate(a.t.name, T(a.t.name))))
 
-      relationTable("#") = S(null, null, null, null, null, attributeTable, null) //todo: hack
-
-      S(name, getCondition(ifExpr, occurrenceTable), null, null, fls map (f => fl2F(f, occurrenceTable)), attributeTable, occurrenceTable)
+      val g: G = getCondition(ifExpr, attributeTable, occurrenceTable)
+      val fs: List[F] = fls map (f => fl2F(f, attributeTable, occurrenceTable))
+      S(name, g, null, null, fs, attributeTable, occurrenceTable)
     }
 
     case Task(name, scheme, in, out) => Q(name, null, null)
     case _ => null
   }
 
-  private def getCondition(ifExpr: IfExpr, nTable: HashMap[String, N]): G = ifExpr match {
+  private def getCondition(ifExpr: IfExpr, aTable: HashMap[String, A], nTable: HashMap[String, N]): G = ifExpr match {
     case null => null
-    case i => condition2G(i.condition, nTable)
+    case i => condition2G(i.condition, aTable, nTable)
   }
 
   private def getOccurrences(expr: Expression): List[AttributeOccurrence] = expr match {
@@ -55,25 +54,27 @@ object Converter {
     case Operator(left, right, op) => getOccurrences(left) ::: getOccurrences(right)
   }
 
-  private def occurrence2N(n: AttributeOccurrence, nTable: HashMap[String, N]): N = n match {
+  private def occurrence2N(n: AttributeOccurrence, aTable: HashMap[String, A], nTable: HashMap[String, N]): N = n match {
     case AttributeOccurrence(attr, null) =>
       nTable.getOrElseUpdate(n.toString,
-        N(relationTable("#").getA(attr), null, new ArrayBuffer[F], new ArrayBuffer[F]))
+        N(aTable(attr), null, new ArrayBuffer[F], new ArrayBuffer[F]))
     case AttributeOccurrence(attr, sub) =>
       nTable.getOrElseUpdate(n.toString,
-        N(relationTable("#").getA(attr), relationTable(relationTable("#").getA(attr).t.name).getA(sub), new ArrayBuffer[F], new ArrayBuffer[F]))
+        N(aTable(attr), relationTable(aTable(attr).t.name).getA(sub), new ArrayBuffer[F], new ArrayBuffer[F]))
   }
 
-  private def expression2X(e: Expression, nTable: HashMap[String, N]): X = X(e toString, getOccurrences(e) map (n => occurrence2N(n, nTable)))
+  private def expression2X(e: Expression, aTable: HashMap[String, A], nTable: HashMap[String, N]): X =
+    X(e toString, getOccurrences(e) map (n => occurrence2N(n, aTable, nTable)))
 
-  private def fl2F(fl: FL, nTable: HashMap[String, N]): F = {
-    val result = F(expression2X(fl.implementation, nTable), occurrence2N(fl.result, nTable))
+  private def fl2F(fl: FL, aTable: HashMap[String, A], nTable: HashMap[String, N]): F = {
+    val result = F(expression2X(fl.implementation, aTable, nTable), occurrence2N(fl.result, aTable, nTable))
     result.res.left += result
     result.expr.args foreach (n => n.right += result)
     return result
   }
 
-  private def condition2G(condition: Expression, nTable: HashMap[String, N]): G = G(expression2X(condition, nTable))
+  private def condition2G(condition: Expression, aTable: HashMap[String, A], nTable: HashMap[String, N]): G =
+    G(expression2X(condition, aTable, nTable))
 
   //  private def block2V(block: Block): V = V()
 }

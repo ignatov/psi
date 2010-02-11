d52f7b0a6082a5aecb9ecf4bbd5887fb70e0f7d3
ignatov <sergey@ignatov.spb.su>
2010-02-11 18:02:13 +0300

    added toString to Metamodel inheritors

--- src/metamodel/Metamodel.scala
+++ src/metamodel/Metamodel.scala
@@ -2,6 +2,7 @@ package pcis.metamodel
 
 import compat.Platform.EOL
 import collection.mutable.{ArrayBuffer, HashMap}
+import java.lang.String
 
 /**
  * @author: ignatov
@@ -11,14 +12,16 @@ import collection.mutable.{ArrayBuffer, HashMap}
 /**
  * Base class for metamodel classes. May be need for converter or printer.
  */
-abstract class Metamodel
+abstract class Metamodel {
+  val indent: String = "  "
+}
 
 /**
  * Package
  */
 case class P(name: String, relations: Map[String, R]) extends Metamodel {
   override def toString =
-    "P(" + name + EOL + "  " + (relations.toList map (x => x._1 + " -> " + x._2)).mkString(EOL + "  ") + EOL + ")"
+    "P(" + name + EOL + indent + (relations.toList map (x => x._1 + " -> " + x._2)).mkString(EOL + indent) + EOL + ")"
 }
 
 /**
@@ -33,14 +36,31 @@ trait R extends Metamodel {
  */
 case class S(name: String, condition: G, thenBranch: V, elseBranch: V, fls: List[F], aTable: HashMap[String, A], nTable: HashMap[String, N]) extends R {
   def getA(attr: String): A = aTable(attr)
+
   def getN(attr: String): N = nTable(attr)
+
+  override def toString = {
+    "S(" + EOL +
+      indent * 2 + "_: " + name + EOL +
+      indent * 2 + "g: " + condition + EOL +
+      indent * 2 + "+: " + thenBranch + EOL +
+      indent * 2 + "-: " + elseBranch + EOL +
+      indent * 2 + "f: " + fls.mkString(", ") + EOL +
+      indent * 2 + "a: " + aTable + EOL +
+      indent * 2 + "n: " + nTable + EOL +
+      indent + ")"
+  }
 }
 
 /**
  * Task
  */
 case class Q(name: String, scheme: S, in: List[N], out: List[N]) extends R {
-  override def toString = "Q(" + name + ", " + scheme.name + ", " + in + ", " + out + ")"
+  override def toString = "Q(" + name + EOL +
+    indent * 2 + scheme.name + EOL +
+    indent * 2 + in + EOL +
+    indent * 2 + out + EOL +
+    indent + ")"
 }
 
 /**
@@ -51,19 +71,31 @@ case class G(expr: X) extends Metamodel
 /**
  * Attribute
  */
-case class A(name: String, t: T) extends Metamodel
+case class A(name: String, t: T) extends Metamodel {
+  override def toString = "A(" + name + ": " + t.name + ")"
+}
 
 /**
  * Attribute occurrence
  */
 case class N(name: A, surname: A, left: ArrayBuffer[F], right: ArrayBuffer[F]) extends Metamodel {
   def attrName = name.name + {if (surname != null) "." + surname.name else ""}
+
+  override def toString = "N(" + attrName + "; " + left.mkString(",") + "; " + right.mkString(",") + ")"
 }
 
 /**
  * Variant part
  */
-case class V(fls: List[F], aTable: HashMap[String, A], nTable: HashMap[String, N]) extends Metamodel
+case class V(fls: List[F], aTable: HashMap[String, A], nTable: HashMap[String, N]) extends Metamodel {
+  override def toString = {
+    "V(" + EOL +
+      indent * 5 + "f: " + fls.mkString(", ") + EOL +
+      indent * 5 + "a: " + aTable + EOL +
+      indent * 5 + "n: " + nTable + EOL +
+      indent * 4 + ")"
+  }
+}
 
 /**
  * Functional link

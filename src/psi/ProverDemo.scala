cd1e4ec27ac965f153fca65088b1d2a7d9afba12
ignatov <sergey@ignatov.spb.su>
2010-04-04 01:11:25 +0400

    simplify demo object
    --HG--
    rename : src/psi/PSIDemo.scala => src/psi/ProverDemo.scala

---
+++ src/psi/ProverDemo.scala
@@ -0,0 +1,31 @@
+package psi
+
+import psi.compiler.parser.PSIParser
+import scala.io.Source.fromFile
+import psi.compiler.metamodel.{Q, P, Converter}
+import psi.synthesizer.Prover
+
+/**
+ * User: ignatov
+ * Date: 03.04.2010
+ */
+
+object ProverDemo {
+  def main(args: Array[String]): Unit = {
+    if (args.length != 1)
+      return Console.err.println("Please enter filename")
+
+    val parseResult = PSIParser.parse(PSIParser.P, fromFile(args(0)) mkString)
+    
+    if (!parseResult.successful)
+      return println(parseResult)
+
+    val pack: P = Converter convert parseResult.get
+    for (val relation <- pack.relations.values) {
+      relation match {
+        case task: Q => println(new Prover doProof task)
+        case _ =>
+      }
+    }
+  }
+}

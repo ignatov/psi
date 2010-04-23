c8061e6b586ddce304a82717e4d4baef9c9f2f74
ignatov <sergey@ignatov.spb.su>
2010-04-23 18:26:38 +0400

    Generator commented

--- src/psi/gererator/Generator.scala
+++ src/psi/gererator/Generator.scala
@@ -7,10 +7,19 @@ import psi.synthesizer.datastructs.Procedure
  * Date: 04.04.2010
  */
 
+/**
+ * Trait for target languages generator
+ */
 trait Generator {
   protected val indent: String = "  ";
 
+  /**
+   * We need map for mapping PSI-types to real types on target language 
+   */
   val typeMap: Map[String, String]
 
+  /**
+   * Main generator method: create target language source code for input procedure
+   */
   def generate(procedure: Procedure): String
 }

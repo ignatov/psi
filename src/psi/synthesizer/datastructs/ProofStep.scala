fa0f5e8059d1f6d8a46db04eb9bbd6b3b376b48c
ignatov <sergey@ignatov.spb.su>
2010-04-23 18:13:54 +0400

    ProofStep commented

--- src/psi/synthesizer/datastructs/ProofStep.scala
+++ src/psi/synthesizer/datastructs/ProofStep.scala
@@ -8,12 +8,21 @@ import collection.mutable.ArrayBuffer
  * Date: 03.04.2010
  */
 
+/**
+ * Base class for proof steps
+ */
 abstract class ProofStep
 
+/**
+ * Linear step
+ */
 case class SingleStep(fl: F, reachedAttribute: N) extends ProofStep {
   override def toString = "ProofStep(" + fl + ", " + reachedAttribute.name.name + ")"
 }
 
+/**
+ * Condition step with two lists of single steps
+ */
 case class ConditionStep(guard: G, thenSteps: ArrayBuffer[SingleStep], elseSteps: ArrayBuffer[SingleStep]) extends ProofStep {
   override def toString = "ConditionStep(" + guard.expr.impl + ", " + thenSteps.mkString(", ") + " | " + elseSteps.mkString(", ") + ")"
 }
